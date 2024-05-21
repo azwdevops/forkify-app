5b9dd973788f8c495c6fc85c239c631fa011e373
Zachary Waweru <azwdevops@gmail.com>
2024-05-21 09:19:40 +0300

    removed api key from config and added link to generate api key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SECONDS = 10;
 export const RESULTS_PER_PAGE = 10;
-export const API_KEY = "dadf3f40-6604-4b9b-96a3-ee9305ce51d2";
+export const API_KEY = ""; // generate api key from here https://forkify-api.herokuapp.com/v2
 export const MODAL_CLOSE_SECONDS = 2.5;
